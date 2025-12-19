1943fa4af041ba9506a264dd8262b44c2403e53f
Gabriel <Gabriel.bachelor@gmail.com>
2025-12-19 18:37:13 +0000

    fixed catch all routs

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,14 @@ app.use("/api/users", usersRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.use((req, res, next) => {
+  if (!req.path.startsWith("/api")) {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
   const error = new HttpError("Could not find this route", 404);
   throw error;
 });
